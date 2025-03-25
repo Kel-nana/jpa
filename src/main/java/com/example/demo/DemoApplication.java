9eaaee7ccae1c8a522cb1cdda53430b2a5ea95ce
Kelvin Irungu <112465233+Kel-nana@users.noreply.github.com>
2025-03-25 18:52:52 +0300

    add controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.client.Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +19,17 @@ public class DemoApplication {
 	}
 
 	@GetMapping()
-	public String hello() {
-		return "Hello World";
+	public List<Client> hello () {
+		return List.of(
+				new Client(
+						"mawai@gmail.com",
+						LocalDate.of(2000, Month.MARCH,27),
+						27119287L,
+						"Mwai",
+						"Kelvin",
+						1L
+				)
+		);
 	}
 
 }
