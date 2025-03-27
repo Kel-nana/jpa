6411041c09066daa6f592a275ae1cf966172f7e2
Kelvin Irungu <112465233+Kel-nana@users.noreply.github.com>
2025-03-27 22:35:24 +0300

    code clean up

--- src/main/java/com/example/demo/client/ClientService.java
+++ src/main/java/com/example/demo/client/ClientService.java
@@ -1,12 +1,8 @@
 package com.example.demo.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 import java.util.Optional;
 
