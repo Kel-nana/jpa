d8d327238afb9e3759054187f2df9e247b45a3b9
Kelvin Irungu <112465233+Kel-nana@users.noreply.github.com>
2025-04-03 22:48:48 +0300

    add JSON input restAPI controller

--- src/main/java/com/example/demo/client/ClientController.java
+++ src/main/java/com/example/demo/client/ClientController.java
@@ -1,10 +1,7 @@
 package com.example.demo.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,5 +25,10 @@ public class ClientController {
     @GetMapping("/{id}")
     public Optional<Client> getClientById(@PathVariable Long id) {
         return clientService.getClientById(id);
+    }
+
+    @PostMapping()
+    public Client addClient(@RequestBody Client client) {
+        return clientService.saveClient(client);
     }
 }
